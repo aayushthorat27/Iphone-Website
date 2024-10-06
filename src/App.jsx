0115d2906a57d9a8195146703c0b2d8fd4791870
Aayush Thorat <aayush.thorat27@gmail.com>
2024-10-06 17:25:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import Navbar from "/components/Navbar"
-import Hero from "/components/Hero"
-import Highlights from "/components/Highlights"
-import Model from "/components/Model"
+import Navbar from "./Navbar"
+import Hero from "./Hero"
+import Highlights from "./Highlights"
+import Model from "./Model"
 
 
 const App = () => {
