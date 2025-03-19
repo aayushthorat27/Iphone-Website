78d37dce64cd7e74123d5a0dc39656060588b2f3
Aayush Thorat <aayush.thorat27@gmail.com>
2025-03-19 11:59:48 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
 import Model from "./components/Model"
 import Footer from "./components/Footer"
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
 
@@ -13,6 +14,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Footer />
+      <Analytics />
     </main>
   )
 }
