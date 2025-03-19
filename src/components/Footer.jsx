576454fcd1dd2b235e372cfb1b88b04aa22a444c
Aayush Thorat <aayush.thorat27@gmail.com>
2025-03-19 21:20:41 +0530

    updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,12 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <div class="container">
-            <footer class="py-5">
-                <div class="row">
+        <div className="container bg-zinc w-screen overflow-hidden">
+            <footer class="p-10">
+                <h2 className="font-bold text-[32px]">iPhone</h2>
+                <div class="flex items-center gap-5 py-5 ">
                     <div class="col-6 col-md-2 mb-3">
-                        <h5>Section</h5>
+                        <h4>Section</h4>
                         <ul class="nav flex-column">
                         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li>
                         <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li>
@@ -51,13 +52,14 @@ const Footer = () => {
       </div> */}
                 </div>
 
-                <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-                    <p>© 2024 Company, Inc. All rights reserved.</p>
-                    <ul class="list-unstyled d-flex">
-                        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
-                        <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
+                <div class="d-flex flex-column flex-sm-row justify-center items-center py-4 my-4 border-top">
+                    <p className="col-6">Copyright © 2025 Apple Inc. All rights reserved.</p>
+                    <p className="col-6">Twiiter</p>
+                    {/* <ul class="list-unstyled d-flex flex-column ">
+                        <li class="ms-3"><a class="link-body-emphasis" href="#">Twitter<svg class="bi" width="24" height="24"><use xlink:href="#twitter"></use></svg></a></li>
+                        <li class="ms-3"><a class="link-body-emphasis" href="#">Insta<svg class="bi" width="24" height="24"><use xlink:href="#instagram"></use></svg></a></li>
                         <li class="ms-3"><a class="link-body-emphasis" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"></use></svg></a></li>
-                    </ul>
+                    </ul> */}
                 </div>
             </footer>
         </div>
